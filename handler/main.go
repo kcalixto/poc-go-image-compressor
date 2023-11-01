ceae190459627a184ae2aaf2a2012d24926d73c7
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-11-01 14:57:37 -0300

    another commit

--- handler/main.go
+++ handler/main.go
@@ -4,15 +4,17 @@ import (
 	"bytes"
 	"crypto/tls"
 	"fmt"
+	"image"
+	"image/jpeg"
+	_ "image/png"
 	"io"
 	"net/http"
-	"strings"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/disintegration/imaging"
-	"github.com/google/uuid"
+	// "github.com/disintegration/imaging"
 )
 
 const (
@@ -23,43 +25,94 @@ const (
 func main() {
 	defer fmt.Println("finished")
 
-	fmt.Println("started conversion 5mb image")
+	// fileName := "brazil_id_image_front (1).png"
+	// image, err := os.ReadFile(fileName)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
 	image, err := GetImage(MB5)
 	if err != nil {
 		panic(err)
 	}
-	err = CompressImage(image, 1024)
+
+	out, err := CompressImage(image)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("started conversion 20mb image")
-	image, err = GetImage(MB5)
-	if err != nil {
-		panic(err)
-	}
-	err = CompressImage(image, 1024)
+	err = SaveLocal(fmt.Sprintf("compressed"), out)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func CompressImage(image []byte, newSize int) error {
-	src, err := imaging.Decode(bytes.NewReader(image))
+// func main() {
+// 	defer fmt.Println("finished")
+
+// 	fileName := "brazil_id_image_front (1)"
+// 	image, err := os.ReadFile(fileName)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	out, err := CompressImage(image, 1024)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	err = SaveLocal(fmt.Sprintf("zbox-9-nopng_%s", fileName), out)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	fileName = "brazil_id_image_back (1)"
+// 	image, err = os.ReadFile(fileName)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	out, err = CompressImage(image, 1024)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	err = SaveLocal(fmt.Sprintf("zbox-9-nopng_%s", fileName), out)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// }
+
+// func CompressImage(image []byte, newSize int) ([]byte, error) {
+// 	src, err := imaging.Decode(bytes.NewReader(image), imaging.AutoOrientation(true))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	out := new(bytes.Buffer)
+// 	resizedImage := imaging.Resize(src, newSize, 0, imaging.Box)
+
+// 	err = imaging.Encode(out, resizedImage, imaging.PNG, imaging.PNGCompressionLevel(9))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	return out.Bytes(), nil
+// }
+
+func CompressImage(source []byte) ([]byte, error) {
+	img, _, err := image.Decode(bytes.NewReader(source))
 	if err != nil {
 		panic(err)
 	}
 
-	out := new(bytes.Buffer)
-	resizedImage := imaging.Resize(src, newSize, 0, imaging.Lanczos)
-
-	err = imaging.Encode(out, resizedImage, imaging.PNG)
+	var compressedImage bytes.Buffer
+	err = jpeg.Encode(&compressedImage, img, &jpeg.Options{Quality: 65})
 	if err != nil {
 		panic(err)
 	}
 
-	filename := strings.Replace(uuid.New().String(), "-", "", -1) + ".png"
-	return SaveInS3(filename, out.Bytes())
+	return compressedImage.Bytes(), nil
 }
 
 func SaveInS3(filename string, imageBytes []byte) error {
@@ -78,6 +131,20 @@ func SaveInS3(filename string, imageBytes []byte) error {
 		Body:   bytes.NewReader(imageBytes),
 		Key:    aws.String(filename),
 	})
+	if err != nil {
+		panic(err)
+	}
+
+	return nil
+}
+
+func SaveLocal(file string, imageBytes []byte) error {
+	writer, err := os.Create(file)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = writer.Write(imageBytes)
 	if err != nil {
 		panic(err)
 	}
